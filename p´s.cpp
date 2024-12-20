f23056718cd348c96442c574b4b1f08986b70f51
RafaelRibeiro19 <rafadoseles@gmail.com>
2024-12-20 16:06:51 +0000

    Add files via upload

---
+++ p´s.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void main()
+{
+	int palavra, i, nletras;
+	char vogais[5] = { 'a','e','i','o','u' }, vogais2[5] = { 'pa','pe','pi','po','pu' }, palavra2[99], p;
+
+	cout << "da um numero de letras\n";
+	cin >> nletras;
+
+	for (i = 0;i < nletras;i++)
+	{
+		cout << "soletra a palavra\n";
+		cin >> palavra2[i];
+		palavra2[i] +palavra;
+	}
+	if (vogais[i] == palavra)
+	{
+		vogais[i] + vogais2[i];
+	}
+
+
+}
